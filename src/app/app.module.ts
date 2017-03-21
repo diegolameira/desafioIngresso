b2139b2141a866b969c241fb9f0280abd8d974a5
Diego Lameira <diegolameira@me.com>
2017-03-21 16:52:17 -0300

    header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LogoComponent } from './shared/logo/logo.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogoComponent
+    LogoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
